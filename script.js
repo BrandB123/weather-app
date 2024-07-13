703a0d3124c007a239265de859789a4cf9953bf5
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2024-07-13 00:52:35 -0500

    Implement dynamic data application to screen

--- script.js
+++ script.js
@@ -21,11 +21,11 @@ function processWeatherData(data){
 
     const currentConditions = {
         currentWeather : data.currentConditions.conditions,
-        feelsLike : data.currentConditions.feelslike,
+        feelsLike : Math.round(data.currentConditions.feelslike),
         icon : data.currentConditions.icon,
         precipProb : data.currentConditions.precipprob,
         precipType : data.currentConditions.precipType,
-        temperature : data.currentConditions.temp
+        temperature : Math.round(data.currentConditions.temp)
     }
 
     const threeDayForecast = {
@@ -37,6 +37,49 @@ function processWeatherData(data){
     return {location, currentConditions, threeDayForecast};
 }
 
+function updateScreen(data){
+    let location = document.querySelector(".title");
+    // let searchInput = document.querySelector(".search-input");
+    let currentIcon = document.querySelector(".current-icon");
+    let temp = document.querySelector(".temp");
+    let feelsLike = document.querySelector(".feels-like");
+    let precipPercent = document.querySelector(".precip-percent");
+    let precipType = document.querySelector(".precip-type");
+
+    let dayOneIcon = document.querySelector(".day-one.forecast-icon");
+    let dayOneMin = document.querySelector(".day-one.forecast-tempmin");
+    let dayOneMax = document.querySelector(".day-one.forecast-tempmax");
+
+    let dayTwoIcon = document.querySelector(".day-two.forecast-icon");
+    let dayTwoMin = document.querySelector(".day-two.forecast-tempmin");
+    let dayTwoMax = document.querySelector(".day-two.forecast-tempmax");
+
+    let dayThreeIcon = document.querySelector(".day-three.forecast-icon");
+    let dayThreeMin = document.querySelector(".day-three.forecast-tempmin");
+    let dayThreeMax = document.querySelector(".day-three.forecast-tempmax");
+
+    location.textContent = data.location;
+    currentIcon.textContent = data.currentConditions.icon;
+    temp.textContent = `${data.currentConditions.temperature}°`;
+    temp.style.marginLeft = "12%";
+    feelsLike.textContent = `feels like ${data.currentConditions.feelsLike}°`;
+    precipPercent.textContent = `${data.currentConditions.precipProb}%`;
+    precipType.textContent = data.currentConditions.precipType;
+
+    dayOneIcon.textContent = data.threeDayForecast.dayOne.icon;
+    dayOneMin.textContent = `${data.threeDayForecast.dayOne.tempmin}°`;
+    dayOneMax.textContent = `${data.threeDayForecast.dayOne.tempmax}°`;
+
+    dayTwoIcon.textContent = data.threeDayForecast.dayTwo.icon;
+    dayTwoMin.textContent = `${data.threeDayForecast.dayTwo.tempmin}°`;
+    dayTwoMax.textContent = `${data.threeDayForecast.dayTwo.tempmax}°`;
+
+    dayThreeIcon.textContent = data.threeDayForecast.dayThree.icon;
+    dayThreeMin.textContent = `${data.threeDayForecast.dayThree.tempmin}°`;
+    dayThreeMax.textContent = `${data.threeDayForecast.dayThree.tempmax}°`;
+
+}
+
 
 //let unprocessedData = getWeatherData("Columbia, MO");
 // let unprocessedData = getWeatherData("38.9525, -92.3342");
@@ -45,5 +88,6 @@ getWeatherData("Columbia, Mo").then((data) => {
     console.log(data);
     const processedData = processWeatherData(data);
     console.log(processedData);
+    updateScreen(processedData);
 })
 
